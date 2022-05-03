c70f69e19bb7415dc0368dc567bbf1bd5d108fb8
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-02 18:26:28 -0700

    feat(Widget): added group class to modify behavior of child component on hover with group-hover

--- frontend-web/src/components/Widget/index.tsx
+++ frontend-web/src/components/Widget/index.tsx
@@ -5,10 +5,10 @@ export function Widget() {
     <div className="absolute bottom-5 right-5">
       <button
         type="button"
-        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center"
+        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
       >
         <ChatTeardropDots className="w-6 h-6" />
-        <span className="max-w-0 overflow-hidden">
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs">
           <span className="pl-2" />
           Feedback
         </span>
