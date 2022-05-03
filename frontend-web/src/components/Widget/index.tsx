e4d9edbf99986e9c756e8405ef6aff8ad8283595
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-02 18:24:10 -0700

    feat(Widget): created button with icon

--- frontend-web/src/components/Widget/index.tsx
+++ frontend-web/src/components/Widget/index.tsx
@@ -1,3 +1,18 @@
+import { ChatTeardropDots } from 'phosphor-react';
+
 export function Widget() {
-  return <h1>Widget</h1>;
+  return (
+    <div className="absolute bottom-5 right-5">
+      <button
+        type="button"
+        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center"
+      >
+        <ChatTeardropDots className="w-6 h-6" />
+        <span className="max-w-0 overflow-hidden">
+          <span className="pl-2" />
+          Feedback
+        </span>
+      </button>
+    </div>
+  );
 }
