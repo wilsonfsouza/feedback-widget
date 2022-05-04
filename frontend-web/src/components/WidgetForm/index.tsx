dc75741d2a66c7114d42c0cb67138573ffda1391
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-03 21:15:40 -0700

    feat(WidgetForm): created function to restart feedback type

--- frontend-web/src/components/WidgetForm/index.tsx
+++ frontend-web/src/components/WidgetForm/index.tsx
@@ -9,6 +9,10 @@ export function WidgetForm() {
     setFeedbackType(type);
   }, []);
 
+  const handleRestartFeedbackType = useCallback(() => {
+    setFeedbackType(null);
+  }, []);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {!feedbackType ? (
