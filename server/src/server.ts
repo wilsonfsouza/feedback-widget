8c1fc2098178bc8d75be6cb77c3d4eabc68a501a
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-05 18:14:04 -0700

    chore: eslint, prettier, and editorconfig setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
 app.listen(3333, () => {
-  console.log('HTTP server running on PORT: 3333')
-})
+  console.log('HTTP server running on PORT: 3333');
+});
