512136475c8f340d6da08f34f5aaa8d4bdfe3175
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-05 22:55:22 -0700

    refactor(routes): removed business logic and data layers from routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,41 +1,8 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
-import SMTPTransport from 'nodemailer/lib/smtp-transport';
-import { prisma } from './prisma';
+import { FeedbacksController } from './infra/controllers/FeedbacksController';
 
 export const routes = express.Router();
 
-const transport = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_HOST),
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-} as SMTPTransport.Options);
+const feedbacksController = new FeedbacksController();
 
-routes.post('/feedbacks', async (req, res) => {
-  const { comment, type, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      comment,
-      type,
-      screenshot,
-    },
-  });
-
-  await transport.sendMail({
-    from: 'Team Feedget <hello@feedget.com>',
-    to: 'Wilson Franca <wilson.franca.92@gmail.com>',
-    subject: 'New feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #222;">`,
-      `<p>Feedback type: ${type}</p>`,
-      `<p>Comment: ${comment}</p>`,
-      `</div>`,
-    ].join('\n'),
-  });
-
-  return res.status(201).json({ data: feedback });
-});
+routes.post('/feedbacks', feedbacksController.create);
