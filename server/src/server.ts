a97707f74fff802e2b2e901d28ecc2ca5dbf62df
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-05 19:47:52 -0700

    feat(routes): added feedback route, saving feedback in the database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,23 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { comment, type, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      comment,
+      type,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
   console.log('HTTP server running on PORT: 3333');
