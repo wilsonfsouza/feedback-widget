d23aa2267ec5266cf3773285de7b00ae969c16ad
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-08 12:26:08 -0700

    feat(Options): created component for select feedback type screen

---
+++ mobile/src/components/Options/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../theme/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  title: {
+    color: theme.colors.text_primary,
+    fontSize: 20,
+    marginBottom: 32,
+    fontStyle: theme.fonts.medium
+  },
+  options: {
+    width: '100%',
+    marginBottom: 48,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
+});
