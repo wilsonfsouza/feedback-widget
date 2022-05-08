0026129324cf99c03be49938a44fe75ee668b10b
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-08 11:32:51 -0700

    feat(widget-mobile): added base style to modal and gestures

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import 'react-native-gesture-handler';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import { theme } from './src/theme/theme';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,16 +1,35 @@
+import React, {useRef} from 'react';
 import { ChatTeardropDots } from 'phosphor-react-native';
-import React from 'react';
 import { TouchableOpacity, View } from 'react-native';
-import { theme } from '../../theme/theme';
+import BottomSheet from '@gorhom/bottom-sheet';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
+import { theme } from '../../theme/theme';
 import { styles } from './styles';
 
-export function Widget() {
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef?.current?.expand();
+  }
+
   return (
     <>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
         <ChatTeardropDots size={24} weight="bold" color={theme.colors.text_on_brand_color}/>
       </TouchableOpacity>
+
+      <BottomSheet 
+        ref={bottomSheetRef} 
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+
+      </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
