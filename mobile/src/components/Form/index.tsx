67917b26a5654a5b4ae59974e1dde62252de133e
Wilson Franca <wilson.franca.92@gmail.com>
2022-05-30 19:37:05 -0700

    feat(Form): added function to convert screenshot and submit feedback to the backend

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { ArrowLeft } from 'phosphor-react-native';
 import { View, TextInput, Image, Text, TouchableOpacity } from 'react-native';
-import { captureScreen } from 'react-native-view-shot'
+import { captureScreen } from 'react-native-view-shot';
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenshotButton } from '../ScreenshotButton';
 import { SubmitFeedbackButton } from '../SubmitFeedbackButton';
@@ -9,14 +10,19 @@ import { SubmitFeedbackButton } from '../SubmitFeedbackButton';
 import { theme } from '../../theme/theme';
 import { styles } from './styles';
 import { feedbackTypes, FeedbackTypes } from '../../utils/feedbackTypes';
+import { api } from '../../libs/api';
 
 interface FormProps {
   feedbackType: FeedbackTypes
+  onFeedbackTypeRemoved: () => void
+  onFeedbackSent: () => void
 }
 
-export function Form({feedbackType}: FormProps) {
+export function Form({feedbackType, onFeedbackSent, onFeedbackTypeRemoved}: FormProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType]
   const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
+  const [comment, setComment] = useState('')
 
   function handleScreenshot() {
     captureScreen({
@@ -31,10 +37,32 @@ export function Form({feedbackType}: FormProps) {
     setScreenshot(null)
   }
 
+  async function handleSendFeedback() {
+    if (isSendingFeedback) {
+      return
+    }
+
+    setIsSendingFeedback(true)
+
+    const screenshotBase64 = screenshot && await FileSystem.readAsStringAsync(screenshot, {encoding: 'base64'})
+    try {
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        screenshot: `data:image/png;base64, ${screenshotBase64}`,
+        comment
+      })
+
+      onFeedbackSent()
+    } catch (error) {
+      console.log(error)
+      setIsSendingFeedback(false)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={onFeedbackTypeRemoved}>
           <ArrowLeft size={24} weight='bold' color={theme.colors.text_secondary} />
         </TouchableOpacity>
         <View style={styles.titleContainer}>
@@ -47,6 +75,7 @@ export function Form({feedbackType}: FormProps) {
         style={styles.input} 
         placeholder="Is something not working well? We want to fix that. Describe with details what is happening to you..." 
         placeholderTextColor={theme.colors.text_secondary}  
+        onChangeText={setComment}
       />
       <View style={styles.footer}>
         <ScreenshotButton
@@ -54,7 +83,7 @@ export function Form({feedbackType}: FormProps) {
           onTakeShot={handleScreenshot}
           screenshot={screenshot}
         />
-        <SubmitFeedbackButton isLoading={false} />
+        <SubmitFeedbackButton isLoading={isSendingFeedback} onPress={handleSendFeedback} />
       </View>
     </View>
   );
